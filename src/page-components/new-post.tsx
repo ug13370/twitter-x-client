a5cc0aac44c50dffba9dbbc1dc71ce6e5c3d378a
Utkarsh Gupta <utkarsh13370@gmail.com>
2023-10-29 18:11:25 +0530

    Frontend UIcompleted for media support

--- src/page-components/new-post.tsx
+++ src/page-components/new-post.tsx
@@ -1,10 +1,20 @@
 import {
+  useRef,
+  useState,
+  forwardRef,
+  useContext,
+  useImperativeHandle,
+} from "react";
+import {
   Box,
   Avatar,
   Divider,
   Popover,
   TextField,
   Typography,
+  ImageList,
+  ImageListItem,
+  Button,
 } from "@mui/material";
 import { LogoComponent } from ".";
 import { ThemeSettings } from "../theme";
@@ -13,7 +23,7 @@ import AppContext from "../utils/contexts/App/AppContext";
 import WallpaperIcon from "@mui/icons-material/Wallpaper";
 import EmojiPicker, { EmojiStyle, Theme } from "emoji-picker-react";
 import InsertEmoticonIcon from "@mui/icons-material/InsertEmoticon";
-import { forwardRef, useContext, useImperativeHandle, useState } from "react";
+import CloseIcon from "@mui/icons-material/Close";
 
 const styles = (themeSettings: any) => {
   return {
@@ -22,6 +32,7 @@ const styles = (themeSettings: any) => {
       flexDirection: "row",
       alignItems: "flex-start",
       padding: "1.2rem",
+      // maxHeight:"20%",
 
       ".avatar": {
         marginRight: "0.7rem",
@@ -57,9 +68,11 @@ const styles = (themeSettings: any) => {
 };
 
 const PostIcons = (props: any) => {
-  const { setTweetContent } = props;
+  const { setTweetContent, handleFileSelections } = props;
 
   const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+
+  const fileRef = useRef<any>(null);
 
   // Emoji popover
   const handleAddEmoticonIconClick = (
@@ -81,6 +94,14 @@ const PostIcons = (props: any) => {
     });
   };
 
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    // Handle the selected file(s) here
+    const selectedFiles = event.target.files;
+
+    // Call your function to handle the selected files
+    handleFileSelections(selectedFiles);
+  };
+
   return (
     <Box className="post-icons">
       <ButtonComponent
@@ -88,6 +109,10 @@ const PostIcons = (props: any) => {
         fullWidth={false}
         borderRadius={10}
         id="insert-medias"
+        onClick={() => {
+          console.log("clicked...");
+          fileRef.current.click();
+        }}
       >
         <WallpaperIcon />
       </ButtonComponent>
@@ -119,6 +144,16 @@ const PostIcons = (props: any) => {
           emojiStyle={EmojiStyle.NATIVE}
         />
       </Popover>
+
+      <input
+        type="file"
+        ref={fileRef}
+        multiple={true}
+        id="selectImages"
+        onChange={handleFileChange}
+        style={{ visibility: "hidden" }}
+        accept="image/jpeg, image/png, image/gif"
+      />
     </Box>
   );
 };
@@ -175,6 +210,41 @@ const PostActions = (props: any) => {
   );
 };
 
+const ImageListComp = (props: any) => {
+  const { imagePreviews = [], removePreview = (name: string) => {} } = props;
+  return (
+    <ImageList
+      gap={10}
+      variant="masonry"
+      sx={{ marginBottom: "0.7rem" }}
+      cols={imagePreviews.length > 1 ? 2 : 1}
+    >
+      {imagePreviews.map((item: any) => (
+        <ImageListItem key={item.src} sx={{ position: "relative" }}>
+          <img
+            src={item.src}
+            loading="lazy"
+            alt={item.title}
+            style={{ borderRadius: 15 }}
+          />
+          <CloseIcon
+            onClick={() => {
+              removePreview(item.name);
+            }}
+            sx={{
+              opacity: 0.5,
+              top: "0.5rem",
+              right: "0.5rem",
+              cursor: "pointer",
+              position: "absolute",
+            }}
+          />
+        </ImageListItem>
+      ))}
+    </ImageList>
+  );
+};
+
 const NewPost = forwardRef((props: any, ref: any) => {
   // Props
   const {
@@ -191,11 +261,13 @@ const NewPost = forwardRef((props: any, ref: any) => {
 
   // States
   const [tweetContent, setTweetContent] = useState("");
+  const [imagePreviews, setImagePreviews] = useState<any>([]);
 
   // Imperatives
   useImperativeHandle(ref, () => ({
     reset() {
       setTweetContent("");
+      setImagePreviews([]);
     },
   }));
 
@@ -204,6 +276,37 @@ const NewPost = forwardRef((props: any, ref: any) => {
     if (e.target.scrollHeight > e.target.clientHeight) {
       e.target.rows = e.target.rows + 1;
     }
+  };
+
+  const handleFileSelections = (selectedFiles: any) => {
+    if (selectedFiles.length + imagePreviews.length > 4) return;
+    for (let i = 0; i < selectedFiles.length; i++) {
+      const file = selectedFiles[i];
+      const reader = new FileReader();
+
+      reader.onload = (event: any) => {
+        const dataURL = event.target.result;
+        const image = {
+          src: dataURL,
+          title: "Image",
+          type: "preview",
+          name: selectedFiles[i].name,
+        };
+
+        setImagePreviews((prevVal: any) => {
+          prevVal.push(image);
+          return [...prevVal];
+        });
+      };
+
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const removePreview = (name: string) => {
+    setImagePreviews((prevVal: any) => {
+      return prevVal.filter((val: any) => val.name !== name);
+    });
   };
 
   return (
@@ -235,9 +338,16 @@ const NewPost = forwardRef((props: any, ref: any) => {
             type === "post" ? "What is happening?" : "Comment on this post"
           }
         />
+        <ImageListComp
+          imagePreviews={imagePreviews}
+          removePreview={removePreview}
+        />
         <Divider className="divider" />
         <Box className="post-actions">
-          <PostIcons setTweetContent={setTweetContent} />
+          <PostIcons
+            setTweetContent={setTweetContent}
+            handleFileSelections={handleFileSelections}
+          />
           <PostActions
             type={type}
             tweetContent={tweetContent}
